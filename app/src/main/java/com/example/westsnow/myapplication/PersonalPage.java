caf9ec410a7a3b70b3e19f2cf6449bc4a524138c
Diyue Xiao <dx2152@columbia.edu>
2015-05-20 18:50:31 -0400

    add timeline

--- app/src/main/java/com/example/westsnow/myapplication/PersonalPage.java
+++ app/src/main/java/com/example/westsnow/myapplication/PersonalPage.java
@@ -6,11 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.*;
